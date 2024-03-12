905b96c9c891c65da91fb97d905889c88d4a1e6d
Somrat-Masud <somratmasud098@gmail.com>
2024-03-12 23:44:44 +0600

    first commit

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import {Outlet
+ } from "react-router-dom";
+import Footer from "../Pages/Home/Home/Shared/Footer/Footer";
+import Navber from "../Pages/Home/Home/Shared/Footer/Navber/Navber";
+
+const Main = () => {
+    return (
+        <div>
+            <Navber></Navber>
+           <Outlet />
+           <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
